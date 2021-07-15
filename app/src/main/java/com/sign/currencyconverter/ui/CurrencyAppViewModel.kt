90636c00c7b03e7318d78809a025e99fece9a085
Serhii Hrihorchuk <gosergio27@gmail.com>
2021-07-15 13:44:43 +0300

    API with handling

--- app/src/main/java/com/sign/currencyconverter/ui/CurrencyAppViewModel.kt
+++ app/src/main/java/com/sign/currencyconverter/ui/CurrencyAppViewModel.kt
@@ -5,35 +5,52 @@ import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.sign.currencyconverter.models.Currency
+import com.sign.currencyconverter.models.Results
 import com.sign.currencyconverter.repository.CurrencyRepository
+import com.sign.currencyconverter.utils.Resource
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class CurrencyAppViewModel(application: Application) : AndroidViewModel(application) {
 
     private val repository : CurrencyRepository
 
-    val TAG = "SAMETON"
-    val currencyRate : MutableLiveData<Double> = MutableLiveData()
+    val currencyRate : MutableLiveData<Resource<Results>> = MutableLiveData()
 
     init {
         repository = CurrencyRepository()
         getCurrentCurrencyCourse("USD_UAH")
     }
 
-
     fun getCurrentCurrencyCourse(query: String){
         viewModelScope.launch(Dispatchers.IO) {
-            val response = repository.getCurrentCurrencyCourse(query)
-            val cur = response.body()
-            val result = cur?.results?.get(query)
-            currencyRate.postValue(result?.value)
+            safeHandleResponse(query)
         }
     }
 
+    private fun handleResponse(response : Response<Currency>,query: String) : Resource<Results> {
+        if(response.isSuccessful){
+            response.body()?.let { currency->
+                val resultsResponse = currency.results.get(query)
+                return Resource.Success(resultsResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
 
+    private suspend fun safeHandleResponse(query :String){
+        try {
+            val response = repository.getCurrentCurrencyCourse(query)
+            currencyRate.postValue(handleResponse(response,query))
+        }
+        catch (t : Throwable){
+            currencyRate.postValue(Resource.Error("Something went wrong"))
+        }
+    }
 
-    fun setDataInfo() {
-
+    fun roundTwoDigits(number : Double) : String{
+        return String.format("%.2f",number).toDouble().toString()
     }
 }
--- app/src/main/java/com/sign/currencyconverter/ui/MainActivity.kt
+++ app/src/main/java/com/sign/currencyconverter/ui/MainActivity.kt
@@ -2,9 +2,12 @@ package com.sign.currencyconverter.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.accessibility.AccessibilityManager
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.sign.currencyconverter.R
+import com.sign.currencyconverter.utils.Resource
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,8 +19,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         viewModel = ViewModelProvider(this).get(CurrencyAppViewModel::class.java)
 
-        viewModel.currencyRate.observe(this, Observer {
-            tv_now.text = it.toString()
+        viewModel.currencyRate.observe(this, Observer { results->
+            when(results){
+                is Resource.Success -> {
+                    val currencyPrice = results.data?.value as Double
+                    tv_now.text = viewModel.roundTwoDigits(currencyPrice)
+                }
+                is Resource.Error -> {
+                    Toast.makeText(this,results.message,Toast.LENGTH_SHORT).show()
+                }
+            }
         })
 
     }
